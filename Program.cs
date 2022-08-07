cd942125df2058afea2373a845543acb45533030
Anton <ceasar0713@gmail.com>
2022-08-07 21:52:19 +0300

    Создали 2 массива, один массив размером 6, второй длина первого

--- Program.cs
+++ Program.cs
@@ -3,3 +3,10 @@
 либо задать на старте выполнения алгоритма. При решение не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами*/
 
+using System;
+using static System.Console;
+Clear();
+{
+string[] array1 = new string[6] {"567", "2335", "team", "zeus", "ok", "simba"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
