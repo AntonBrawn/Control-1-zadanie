84750633e76318e72e1258bf5776bba05414e732
Anton <ceasar0713@gmail.com>
2022-08-07 21:55:59 +0300

    Создали метод, в котором в цикле происходит проверка условия i<=3

--- Program.cs
+++ Program.cs
@@ -10,3 +10,25 @@ Clear();
 string[] array1 = new string[6] {"567", "2335", "team", "zeus", "ok", "simba"};
 string[] array2 = new string[array1.Length];
 void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int k = 0; k < array.Length; k++)
+    {
+        Console.Write($"{array[k]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
+}
